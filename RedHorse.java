a8b8cd6d6a9b907de373d9d34a6bef10e2c4a570
AllanBalsita <allanbalsita1717@gmail.com>
2021-03-16 21:05:26 -0700

    commits

---
+++ RedHorse.java
@@ -0,0 +1,16 @@
+public class RedHorse{
+  private String raceCompleted;
+
+  public RedHorse(String raceCompleted){
+	setRaceCompleted(raceCompleted);
+
+	}
+  public String getRaceCompleted(){
+	return raceCompleted;
+	}
+  public void setRaceCompleted(String raceCompleted){
+	this.raceCompleted=raceCompleted;
+	}
+
+  
+}
